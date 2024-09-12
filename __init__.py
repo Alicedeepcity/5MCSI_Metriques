8fc98d4a6660a448f69d1db6304b6b60d82a569a
Alicedeepcity <98048050+Alicedeepcity@users.noreply.github.com>
2024-09-12 15:25:36 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -34,6 +34,17 @@ def mongraphique():
 @app.route("/histogramme/")
 def histogramme():
     return render_template("histogramme.html")
+  from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/contact/')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
   
 if __name__ == "__main__":
   app.run(debug=True)
