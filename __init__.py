bf3db1eb2498f65c58a4c9e7af083855d4bb65d5
Alicedeepcity <98048050+Alicedeepcity@users.noreply.github.com>
2024-09-12 15:42:21 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -15,6 +15,10 @@ def hello_world():
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
 
+@app.route("/contact/")
+def MaPremiereAPI():
+    return render_template("contact.html")
+
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
@@ -37,12 +41,7 @@ def histogramme():
   from flask import Flask, render_template
 
 app = Flask(__name__)
-
-@app.route("/contact/")
-def MaPremiereAPI():
-    return render_template("contact.html")
   
-
 if __name__ == '__main__':
     app.run(debug=True)
 
